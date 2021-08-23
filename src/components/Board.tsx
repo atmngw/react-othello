52a81e3dd6bfdf91ab28d7769dd1adbab7313bf2
atsuki <atsuki.minagawa@gmail.com>
2021-08-24 07:40:00 +0900

    Boardを関数コンポーネント化

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -11,39 +11,37 @@ type Props = {
 
 type State = {}
 
-export class Board extends React.Component<Props, State> {
-  renderSquare(position: number): any {
-    const squares = this.props.squares.slice();
+export const Board = (props: Props, state: State) => {
+  const renderSquare = (position: number): any => {
+    const squares = props.squares.slice();
 
     return (
       <Square
         key={position}
         position={position}
         value={squares[position]}
-        onClick={() => this.props.squareClick(position)}/>
+        onClick={() => props.squareClick(position)}/>
     );
   }
 
-  render() {
-    let rows = []
-    for (let row = 1; row <= 8; row++) {
-      let cols = []
-      for (let col = 1; col <= 8; col++) {
-        const position = getPosition(row, col);
-        cols.push(this.renderSquare(position))
-      }
-
-      rows.push(<tr className="line" key={row} data-row={row}>{cols}</tr>)
+  let rows = []
+  for (let row = 1; row <= 8; row++) {
+    let cols = []
+    for (let col = 1; col <= 8; col++) {
+      const position = getPosition(row, col);
+      cols.push(renderSquare(position))
     }
 
-    return (
-      <div className='board'>
-        <table className="table">
-          {rows}
-        </table>
-      </div>
-    );
+    rows.push(<tr className="line" key={row} data-row={row}>{cols}</tr>)
   }
+
+  return (
+    <div className='board'>
+      <table className="table">
+        {rows}
+      </table>
+    </div>
+  );
 }
 
 export type Position = number;
