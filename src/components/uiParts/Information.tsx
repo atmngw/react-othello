d9bc763746bc84435293cfdb285b577518a1117f
atsuki <atsuki.minagawa@gmail.com>
2022-02-10 23:21:10 +0900

    #20 InformationコンポーネントからGameResultを切り出し

--- src/components/uiParts/Information.tsx
+++ src/components/uiParts/Information.tsx
@@ -1,35 +1,6 @@
 import React from 'react';
 import {STONES, toDisplay} from 'utils/Stone';
-
-type GameResultProps = {
-  countColors: { [key: number]: number };
-  isFinished: boolean;
-}
-
-/**
- * 勝敗の表示
- * @constructor
- */
-const GameResult: React.FC<GameResultProps> = ({countColors, isFinished}) => {
-  const blackCount = countColors[STONES.BLACK]
-  const whiteCount = countColors[STONES.WHITE]
-
-  let tag = null;
-
-  if (isFinished) {
-    if (blackCount === whiteCount) {
-      tag = <div className={'game-end'}>引き分け</div>
-    } else if (blackCount > whiteCount) {
-      tag = <div className={'game-end'}>BLACKの勝ち</div>
-    } else {
-      tag = <div className={'game-end'}>WHITEの勝ち</div>
-    }
-  }
-
-  return (
-    tag
-  );
-}
+import {GameResult} from "./GameResult";
 
 type InformationProps = {
   currentlyColor: number;
